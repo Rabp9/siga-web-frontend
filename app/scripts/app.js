a45f57ab793f157f4001855d2af971f669a635de
tmtrbocanegra <rabp_91@hotmail.com>
2018-01-22 15:53:33 -0500

    Primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sigaWebFrontendApp
+ * @description
+ * # sigaWebFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sigaWebFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sigaWebFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sigaWebFrontendApp
+ */
+angular.module('sigaWebFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
